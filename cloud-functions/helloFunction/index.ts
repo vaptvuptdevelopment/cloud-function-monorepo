512cfee50087bedec9d4cc0c85895ad25556cb1b
Vinícius <viniciusmvn@pm.me>
2023-10-02 03:12:54 -0300

    add missing header

--- cloud-functions/helloFunction/index.ts
+++ cloud-functions/helloFunction/index.ts
@@ -1,5 +1,6 @@
 import { HttpFunction } from '@google-cloud/functions-framework';
 
 export const helloFunction: HttpFunction = async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello World!');
 }
